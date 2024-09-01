47f9fc546192d4120505d3ac204b0172c9e651af
ElisandraMeyer <elisandramyr@gmail.com>
2024-09-01 17:01:39 -0300

    Teste

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,6 +30,10 @@ ALLOWED_HOSTS = [
     '127.0.0.1'
 ]
 
+CSRF_TRUSTED_ORIGINS = ["https://controle-tarefas-backend-production.up.railway.app/"]
+CSRF_ALLOWED_ORIGINS = ["https://controle-tarefas-backend-production.up.railway.app/"]
+CORS_ORIGINS_WHITELIST = ["https://controle-tarefas-backend-production.up.railway.app/"]
+
 
 # Application definition
 
